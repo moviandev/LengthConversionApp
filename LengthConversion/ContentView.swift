e6d2dc7e7fd836727abbe26a8727d74b87ceec3e
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-13 00:19:39 -0300

    Create first scrtucture to receive input

--- LengthConversion/ContentView.swift
+++ LengthConversion/ContentView.swift
@@ -7,15 +7,36 @@
 
 import SwiftUI
 
+enum Units {
+    case Km, Ft, Yd, Mi
+}
+
 struct ContentView: View {
+    @State private var inputValue: Double = 0
+    @State private var selectedInputUnit: String = "Km"
+    @State private var selectedOutputUnit: String = "Ft"
+    
+    let units: [String] = ["Km", "Ft", "Yd", "Mi"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Value", value: $inputValue, format: .number)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Unit", selection: $selectedInputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Input Value and Select Unit")
+                }
+            }
+            .navigationTitle("Length Conversion")
         }
-        .padding()
     }
 }
 
