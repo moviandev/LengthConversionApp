ae7b3517ac5873d04ce9a653ae23b15d24e10323
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-12 23:42:12 -0300

    First commit

---
+++ LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by Matheus Viana on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
