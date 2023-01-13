c2de378fa8585e8e97b742565578bc8b00701487
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-13 00:21:06 -0300

    Add output text and segmented picker

--- LengthConversion/ContentView.swift
+++ LengthConversion/ContentView.swift
@@ -7,16 +7,14 @@
 
 import SwiftUI
 
-enum Units {
-    case Km, Ft, Yd, Mi
-}
-
 struct ContentView: View {
     @State private var inputValue: Double = 0
     @State private var selectedInputUnit: String = "Km"
     @State private var selectedOutputUnit: String = "Ft"
     
     let units: [String] = ["Km", "Ft", "Yd", "Mi"]
+    
+    var output: Double = 0
     
     var body: some View {
         NavigationView {
@@ -33,6 +31,18 @@ struct ContentView: View {
                     .pickerStyle(.segmented)
                 } header: {
                     Text("Input Value and Select Unit")
+                }
+                
+                Section {
+                    Text(output, format: .number)
+                    Picker("Unit", selection: $selectedOutputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Select a unit to input be converted")
                 }
             }
             .navigationTitle("Length Conversion")
